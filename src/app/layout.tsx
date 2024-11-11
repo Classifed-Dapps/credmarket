a3f48c945d79aa6fe95827e5c689e89bd1d218c7
Happiesamuel <114792014+Happiesamuel@users.noreply.github.com>
2024-11-11 15:11:55 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,43 @@ import type { Metadata } from "next";
 import "./globals.css";
 import dynamic from "next/dynamic";
 import Header from "@/components/layout-ui/Header";
+import { Inter, Mansalva, Manrope, Roboto, Poppins } from "next/font/google";
 import Footer from "@/components/layout-ui/Footer";
 
 export const metadata: Metadata = {
   title: "Credlist",
   description: "classified meta",
 };
-
+const mansalva = Mansalva({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "400",
+  variable: "--font-mansalva",
+});
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+  weight: "400",
+});
+const manrope = Manrope({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+  weight: "400",
+});
+const roboto = Roboto({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
+  weight: "400",
+});
+const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+  weight: "400",
+});
 const App = dynamic(() => import("./App"), { ssr: true });
 
 export default function RootLayout({
@@ -18,10 +48,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body
+        className={`${mansalva.variable} ${manrope.variable} ${inter.variable} ${roboto.variable} ${poppins.variable} 
+           flex-col bg-primary-1 my-0 mx-auto max-w-[144rem]  min-h-screen antialiased`}
+      >
         <App>
           <Header />
-          <main className="mt-16">{children}</main>
+          <main className="pt-16">{children}</main>
           <Footer />
         </App>
       </body>
